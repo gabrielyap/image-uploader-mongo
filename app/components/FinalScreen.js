884a1bd66f8a93d2bf17956398c91dff6963ec5a
Gabriel Yap <glyap@ucdavis.edu>
2023-10-24 01:18:09 -0700

    Update FinalScreen.js

--- app/components/FinalScreen.js
+++ app/components/FinalScreen.js
@@ -143,7 +143,7 @@ export default function FinalScreen({ imageUrl, setBefore, setAfter, hasUploaded
             <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
                 {allImages.map((item, index) => (
                     <div className="relative m-auto" key={index}>
-                        <Image cloudName="dnyt3b1h3" publicId={item.imageLink} key={index} className="w-full h-full object-fit-contain" />
+                        <Image cloudName="dnyt3b1h3" publicId={item.imageLink.replace('/upload', '/upload/w_300')} key={index} className="w-full h-full object-fit-contain" />
                         <div className="absolute inset-0 flex items-center justify-center opacity-0 bg-black bg-opacity-50 cursor-pointer hover:opacity-100" onMouseEnter={() => setEditedLabel(item.label)} onMouseLeave={() => exitImage()}>
                             {editMode ? (
                                 <input
