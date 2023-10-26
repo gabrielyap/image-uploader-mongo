610da777c929fa4b99028e79632035cb7e33b559
Gabriel Yap <glyap@ucdavis.edu>
2023-10-25 19:52:34 -0700

    Update image.js

--- backend/models/image.js
+++ backend/models/image.js
@@ -1,12 +1,18 @@
+
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 //For now only store link, no title
-
+const CommentSchema = new Schema({
+    content: String,
+    author: String,
+})
 const ImageSchema = new Schema({
     imageLink: String,
     label: String,
     author: String,
+    comments: [CommentSchema]
 })
 
 module.exports = mongoose.model('Image', ImageSchema)
+module.exports = mongoose.model('Comment', CommentSchema)
